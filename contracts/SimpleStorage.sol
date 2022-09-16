d9395dc890e373746ee5c9a599576a980c42938c
Remix Workspace <>
2022-09-17 00:47:38 +0530

    Initial commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+contract SimpleStorage
+{
+    uint256 favouriteNumber;
+    bool favouriteBool;
+
+    struct People
+    {
+        uint256 favouriteNumber;
+        string name;        
+    }
+
+    People[] public people;
+
+    function store(uint256 _favouriteNumber) public
+    {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    function retrieve() public view returns(uint256)
+    {
+        return favouriteNumber;
+    } 
+
+    function addPerson(string memory _name,uint256 _favouriteNumber) public 
+    {
+        people.push(People(_favouriteNumber,_name));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+
+
+}
+
---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+//importing Simple storage contract
+import "./SimpleStorage.sol";
+contract StorageFactory
+{
+
+}
