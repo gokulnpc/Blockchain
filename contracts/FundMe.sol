2765b3fa3a65284052a9337470d7e6c2e9af7178
Remix Workspace <>
2022-09-17 16:44:33 +0530

    Safe Math

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,49 +2,33 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
-//if you want to interact with an already deployed smart contract / interface contract then you will need an ABI
-
-// interface AggregatorV3Interface {
-//   function decimals() external view returns (uint8);
-
-//   function description() external view returns (string memory);
-
-//   function version() external view returns (uint256);
-
-//   function getRoundData(uint80 _roundId)
-//     external
-//     view
-//     returns (
-//       uint80 roundId,
-//       int256 answer,
-//       uint256 startedAt,
-//       uint256 updatedAt,
-//       uint80 answeredInRound
-//     );
-
-//   function latestRoundData()
-//     external
-//     view
-//     returns (
-//       uint80 roundId,
-//       int256 answer,
-//       uint256 startedAt,
-//       uint256 updatedAt,
-//       uint80 answeredInRound
-//     );
-// }
-
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 contract FundMe
 { 
    
+    //it will use safemathchainlink for all of uint256
+    //it doesn't allow overflow to occur 
+    using SafeMathChainlink for uint256;
+    
     mapping(address => uint256) public addressToAmountFunded;
     
+    address owner;
+    constructor()
+    {
+        owner = msg.sender;
+    }
     //we can send value because it is payable
     //msg.sender => my account public address
     //msg.value => gas value that I allocated
     function fund() public payable
     {
+        //minimum 50$
+        uint256 minimumUSD = 50*10**18;
+
+        //if it is not satisfied then it will stop executing and print error message
+        require(getConversionRate(msg.value) >= minimumUSD,"You need to spend more ETH");
+
+
         addressToAmountFunded[msg.sender] += msg.value;
         //what the USD to ETH conversion rate 
     }
@@ -77,5 +61,19 @@ contract FundMe
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice*_ethAmount)/1000000000000000000;
         return ethAmountInUsd;
+    }
+
+    function withDraw(uint256 _amount) payable public
+    {
+        //require msg.sender == owner
+        require(msg.sender == owner,"Only owner can withdraw");
+
+        //transfer function transfer fund from one address to another
+        //here we are transferrring to msg.sender (me)
+        //to transfer we need addresses
+        //this refers to the contract that we are currently in
+        //address(this) gives adress of the current contract
+        //address.balance gives balance in that address
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
