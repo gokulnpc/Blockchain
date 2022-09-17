ce3e8355780acb4299d85593fdcbb8310925607d
Remix Workspace <>
2022-09-17 19:12:58 +0530

    Safe Math

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,40 +5,25 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 contract FundMe
 { 
-   
-    //it will use safemathchainlink for all of uint256
-    //it doesn't allow overflow to occur 
     using SafeMathChainlink for uint256;
-    
     mapping(address => uint256) public addressToAmountFunded;
-    
     address owner;
+    address[] public funders;
+
     constructor()
     {
         owner = msg.sender;
     }
-    //we can send value because it is payable
-    //msg.sender => my account public address
-    //msg.value => gas value that I allocated
     function fund() public payable
     {
-        //minimum 50$
-        uint256 minimumUSD = 50*10**18;
-
-        //if it is not satisfied then it will stop executing and print error message
+        uint256 minimumUSD = 50*10*18;
         require(getConversionRate(msg.value) >= minimumUSD,"You need to spend more ETH");
-
-
         addressToAmountFunded[msg.sender] += msg.value;
-        //what the USD to ETH conversion rate 
+        funders.push(msg.sender);
     }
 
     function getVersion() public view returns(uint256)
     {
-        //AggregatorV3Interface is not a contract (Class)
-        //it is a interface
-        //we need ETH to USD address 
-        //we can get it from 'ethereum price feeds'
         AggregatorV3Interface pricefeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
         return pricefeed.version();  
     }
@@ -63,17 +48,23 @@ contract FundMe
         return ethAmountInUsd;
     }
 
-    function withDraw(uint256 _amount) payable public
-    {
-        //require msg.sender == owner
-        require(msg.sender == owner,"Only owner can withdraw");
 
-        //transfer function transfer fund from one address to another
-        //here we are transferrring to msg.sender (me)
-        //to transfer we need addresses
-        //this refers to the contract that we are currently in
-        //address(this) gives adress of the current contract
-        //address.balance gives balance in that address
+    //withdraw function to withdraw all ethers from the contract
+    //modifiers: to chainge function in a declarative way
+    modifier onlyOwner
+    {
+        require(msg.sender == owner,"Only owner can withdraw");
+        _;
+    }
+    function withDraw() payable onlyOwner public
+    {
+        //require(msg.sender == owner,"Only owner can withdraw");
+        //Withdraw function
         payable(msg.sender).transfer(address(this).balance);
+        for(uint256 i=0;i<funders.length;i++)
+        {
+            addressToAmountFunded[funders[i]]=0;
+        }
+        funders = new address[](0);
     }
 }
